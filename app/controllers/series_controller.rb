4bd6bed529e6cdd8b2a67503b96746a914407bd4
Jan Oelze <janoelzelebt@googlemail.com>
2012-09-25 15:44:43 +0200

    reformatting

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -48,25 +48,31 @@ class SeriesController < ApplicationController
       episode[:image] = episode_element.css("filename").first.content
       episode[:last_updated] = episode_element.css("lastupdated").first.content.to_i
 
-      since = params[:since].to_i
-
       if episode[:date] != ''
+        
         episode[:unixdate] = Time.parse(episode[:date]).to_i
+        since = params[:since].to_i
+
         if Time.parse(episode[:date]).to_i > since
           series[:episodes].push episode
         end
+      
       end
 
     end
 
     if !params[:episode]
       series[:banners] = []
+      
       banners.css("Banner").each do |banner|
+        
         if banner.css("BannerType").first.content == "series" &&
           banner.css("BannerType2").first.content == "graphical"
           series[:banners].push banner.css("BannerPath").first.content
         end
+
       end
+    
     end
 
     if params["season"].present?
